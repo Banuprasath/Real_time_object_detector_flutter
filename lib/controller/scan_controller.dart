8707e8d639a5752c33f2243eb844130b2954bfa9
BanuPrasath <banuprasath0339@gmail.com>
2023-07-08 09:13:46 +0530

    pharse two

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,12 +1,15 @@
 import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
+//import 'package:tflite/tflite.dart';
 
 class ScanController extends GetxController {
   @override
   void onInit() {
     super.onInit();
     initCamera();
+    initTflite();
   }
 
   @override
@@ -19,17 +22,60 @@ class ScanController extends GetxController {
   late List<CameraDescription> Cameras;
 
   var isCameraInitialized = false.obs;
+  var cameraCount = 0;
 
   initCamera() async {
     if (await Permission.camera.request().isGranted) {
       Cameras = await availableCameras();
       cameraController =
           await CameraController(Cameras[0], ResolutionPreset.max);
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraCount = 0;
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if (cameraCount % 10 == 0) {
+            cameraCount = 0;
+            objectDetector(image);
+          }
+          update();
+        });
+        update();
+      });
       isCameraInitialized(true);
       update();
     } else {
       print("Permission denied");
+    }
+  }
+
+  initTflite() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/label.txt",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+
+    //need to use log
+    if (detector != null) {
+      print("result is $detector");
     }
   }
 }
